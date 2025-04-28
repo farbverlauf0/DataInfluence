f4f3a11589e9619bab08a32b0df494b198bc086e
tiifg <smahtinayana@gmail.com>
2025-04-28 15:35:57 +0300

    shapley sampler without calculation

--- utils/samplers/custom_samplers_2/__init__.py
+++ utils/samplers/custom_samplers_2/__init__.py
@@ -1,11 +1,5 @@
 import numpy as np
-from pydvl.value import compute_shapley_values
-from pydvl.utils import Dataset, Utility
-from pydvl.value.shapley import ShapleyMode, MaxUpdates
-from sklearn.ensemble import HistGradientBoostingRegressor
-from ..base import AbstractSampler, IndexSampler
-
-
+from src.samplers import AbstractSampler, IndexSampler
 
 class ShapleySampler(AbstractSampler):
     def __init__(self, X_test, y_test, num_samples=100):
@@ -13,24 +7,12 @@ class ShapleySampler(AbstractSampler):
         self.X_test = X_test
         self.y_test = y_test
         self.index_sampler = IndexSampler()
+        self.sh_values = None
 
-    def __call__(self, X, y, weight, *args, **kwargs):
+    def __call__(self, X, y, sh_values, weight):
 
-        if X.shape[0] != y.shape[0] or X.shape[0] != weight.shape[0]:
-            raise ValueError
+        self.sh_values = sh_values
 
-        dataset = Dataset(x_train=X, y_train=y, x_test=self.X_test, y_test=self.y_test)
-        model = HistGradientBoostingRegressor(max_iter=50, max_depth=3)
-        utility = Utility(model, dataset)
-
-        shap_values = compute_shapley_values(
-            utility,
-            mode=ShapleyMode.PermutationMontecarlo,
-            done=MaxUpdates(10),
-            n_jobs=1,
-            progress=True
-        )
-
-        indices = np.argsort(shap_values)[-self.num_samples:]
+        indices = np.argsort(sh_values.values)[-self.num_samples:]
 
         return self.index_sampler(X, y, weight, index=indices)
